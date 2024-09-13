4fd87ca3b33aa37994e8b2e480ae7e977bf995f9
charu153050 <charukukreja15@gmail.com>
2024-09-14 01:30:13 +0530

    run all function

--- src/components/MidArea.js
+++ src/components/MidArea.js
@@ -1,23 +1,45 @@
 import React, { useState } from "react";
 import DropArea from "./DropArea";
 import Icon from "./Icon";
-
+ 
 export default function MidArea({ activeCard, moveRight, handleRotate }) {
   const [cardIds, setCardIds] = useState([]);
   const onDrop = (position) => {
     // console.log(`${activeCard} at position ${position}`);
     if (activeCard === null || activeCard === undefined) return;
-
+ 
     const newCardsId = [...cardIds];
     newCardsId.splice(position, 0, activeCard);
     setCardIds(newCardsId);
   };
-
+ 
+  const moveRight10Steps = () => moveRight(10);
+  const rotateRightWith15Degree = () => handleRotate(15);
+  const rotateLeftWith15Degree = () => handleRotate(-15);
+ 
+  const runAllFunction = () => {
+    // console.log("fun run");
+    // console.log(cardIds);
+    cardIds.forEach(id => {
+      // console.log(id)
+      if(id == 2) {
+        console.log(id);
+        moveRight10Steps()
+      };
+      if (id == 3) {
+        rotateLeftWith15Degree()
+      };
+      if (id == 4) {
+        rotateRightWith15Degree()
+      };
+    })
+  }
+ 
   const renderCard = (cardId, moveRight) => {
     switch (cardId) {
       case "0":
         return (
-          <div className="flex flex-row flex-wrap bg-yellow-500 text-white px-2 py-1 text-sm cursor-pointer">
+          <div className="flex flex-row flex-wrap bg-yellow-500 text-white px-2 py-1 text-sm cursor-pointer" onClick={runAllFunction}>
             {"When "}
             <Icon name="flag" size={15} className="text-green-600 mx-2" />
             {"clicked"}
@@ -33,7 +55,7 @@ export default function MidArea({ activeCard, moveRight, handleRotate }) {
         return (
           <div
             className="flex flex-row flex-wrap bg-blue-500 text-white px-2 py-1 text-sm cursor-pointer"
-            onClick={() => moveRight(10)}
+            onClick={moveRight10Steps}
           >
             {"Move 10 steps"}
           </div>
@@ -42,7 +64,7 @@ export default function MidArea({ activeCard, moveRight, handleRotate }) {
         return (
           <div
             className="flex flex-row flex-wrap bg-blue-500 text-white px-2 py-1 text-sm cursor-pointer"
-            onClick={() => handleRotate(-15)}
+            onClick={rotateLeftWith15Degree}
           >
             {"Turn "}
             <Icon name="undo" size={15} className="text-white mx-2" />
@@ -53,7 +75,7 @@ export default function MidArea({ activeCard, moveRight, handleRotate }) {
         return (
           <div
             className="flex flex-row flex-wrap bg-blue-500 text-white px-2 py-1 text-sm cursor-pointer"
-            onClick={() => handleRotate(15)}
+            onClick={rotateRightWith15Degree}
           >
             {"Turn "}
             <Icon name="redo" size={15} className="text-white mx-2" />
@@ -64,7 +86,7 @@ export default function MidArea({ activeCard, moveRight, handleRotate }) {
         return null;
     }
   };
-
+ 
   return (
     <div className="h-full overflow-auto">
       <DropArea onDrop={() => onDrop(0)} />
@@ -79,3 +101,4 @@ export default function MidArea({ activeCard, moveRight, handleRotate }) {
     </div>
   );
 }
+ 
