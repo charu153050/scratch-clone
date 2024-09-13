4688490b6f8e12703bf9ee32f68ba3bb81858bf7
charu153050 <charukukreja15@gmail.com>
2024-09-14 01:01:38 +0530

    reset-button

--- src/App.js
+++ src/App.js
@@ -19,6 +19,12 @@ export default function App() {
     setRotation((prevRotation) => prevRotation + angle); // Rotate by 15 degrees
   };
 
+  const resetPage=()=>{
+    setPosition({ x: 0, y: 0 })
+    setRotation(0);
+
+  }
+
   return (
     <div className="bg-blue-100 pt-6 font-sans">
       <div className="h-screen overflow-hidden flex flex-row  ">
@@ -27,7 +33,7 @@ export default function App() {
           <MidArea activeCard={activeCard} moveRight={moveRight} handleRotate={handleRotate} />
         </div>
         <div className="w-1/3 h-screen overflow-hidden flex flex-row bg-white border-t border-l border-gray-200 rounded-tl-xl ml-2">
-          <PreviewArea position={position}  setPosition={setPosition} rotation={rotation} />
+          <PreviewArea position={position}  setPosition={setPosition} rotation={rotation}  resetPage={resetPage}/>
         </div>
       </div>
     </div>
--- src/components/PreviewArea.js
+++ src/components/PreviewArea.js
@@ -1,11 +1,25 @@
 import React from "react";
 import CatSprite from "./CatSprite";
 
-export default function PreviewArea({position, rotation,setPosition}) {
+export default function PreviewArea({
+  position,
+  rotation,
+  setPosition,
+  resetPage,
+}) {
   return (
     <div className="flex-none h-full p-2">
-      <CatSprite position={position} rotation={rotation} setPosition={setPosition} />
-      
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
+        onClick={resetPage}
+      >
+        Reset Button
+      </button>
+      <CatSprite
+        position={position}
+        rotation={rotation}
+        setPosition={setPosition}
+      />
     </div>
   );
 }
